52507fa902488d0a265892e0f8f92456bbfb0c55
Kumud Lekhok <kumudclekhok@gmail.com>
2023-08-04 03:18:33 +0530

    Added to Upper Case Functionality

--- src/Components/Textform/TextForm.tsx
+++ src/Components/Textform/TextForm.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
+
+
+
+
 interface TextFormProps {
     heading :string;
 }
 
 const TextForm : React.FC<TextFormProps> = (props) =>{
+
+    const handleUcClick = (): void => {
+        
+        let newText = text.toUpperCase();
+        setText(newText)
+      };
+
+      const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>):void => {
+        
+        setText(e.target.value)
+      }
+
+    const [text, setText] = useState("Enter Text here");
+
+  
     return (
        
         <div>
@@ -10,9 +31,9 @@ const TextForm : React.FC<TextFormProps> = (props) =>{
 
         <div className="mb-3">
         
-        <textarea className="form-control" id="myBox" rows={8}></textarea>
+        <textarea className="form-control" id="myBox" rows={8} value={text} onChange={handleOnChange}></textarea>
       </div>
-        <button className="btn btn-primary">UPPERCASE</button>
+        <button className="btn btn-primary"  onClick={handleUcClick}>UPPERCASE</button>
       </div>
         
     )
